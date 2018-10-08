1fa1df2ce9a9e28d16cde97956decf5699491cda
Murilo Henrique <murilohns@gmail.com>
2018-10-08 15:15:40 -0300

    adiciona a rota de calculo para somas

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,13 @@ routes.get("/", (req, res) => {
   res.render("index");
 });
 
+routes.get("/calculate", (req, res) => {
+  if (req.body.operator == "+") {
+    result = req.body.number1 + req.body.number2;
+  }
+
+  res.render("index");
+});
+
+
 module.exports = routes;
